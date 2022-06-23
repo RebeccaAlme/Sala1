757c09bc89a4bc646b650420387cf449094d8426
rebecca_alvarez <realvarez.dwfc5@ucamp.utel.edu.mx>
2022-06-23 18:05:19 -0500

    test

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -42,7 +42,7 @@ const blog = () => {
   <blockquote className="blockquote mb-0">
     <p>
       {' '}
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+      Hola ipsum dolor sit amet, consectetur adipiscing elit. Integer
       posuere erat a ante.{' '}
     </p>
     <footer className="blockquote-footer">
